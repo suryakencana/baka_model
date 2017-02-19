cfce019c13984702efd67ff6e1f967168b5749af
Surya 'Zi <worksuryakencana@gmail.com>
2017-02-19 22:14:55 +0700

    EnumInt type add

--- baka_model/model/meta/type.py
+++ baka_model/model/meta/type.py
@@ -16,13 +16,14 @@
  # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  #  type.py
 """
+from collections import Iterable
 import json
+
 import bcrypt
-from sqlalchemy import Numeric, TypeDecorator, String, CHAR, VARCHAR, Unicode
+from sqlalchemy import Numeric, TypeDecorator, String, CHAR, VARCHAR, Unicode, SmallInteger
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.mutable import Mutable
 from sqlalchemy_utils.types import uuid
-
 
 Amount = Numeric(8, 2)
 
@@ -153,3 +154,53 @@ class MutableDict(Mutable, dict):
 
         dict.__delitem__(self, key)
         self.changed()
+
+
+class EnumInt(object):
+    def __init__(self, value, values):
+        self.value = value
+        self._values = values
+
+    @property
+    def title(self):
+        return self.value if self.value is None else self._values[self.value][1]
+
+    @property
+    def key(self):
+        return self.value is not None and self._values[self.value][0]
+
+    def __eq__(self, other):
+        values = list(map(lambda x: x[0], self._values))
+        return (other in values) and self.value == values.index(other)
+
+    def __str__(self):
+        return self.value if self.value is None else u"%s, %s" % (
+            self.value, self._values[self.value][1]
+        )
+
+    def __repr__(self):
+        return u"EnumInt(%s)" % self
+
+    def serialize(self):
+        return {'key': self.key, 'value': self.value, 'title': self.title}
+
+
+class EnumIntType(TypeDecorator):
+
+    impl = SmallInteger
+    values = None
+
+    def __init__(self, values=None):
+        super(EnumIntType, self).__init__()
+        assert values is None or isinstance(values, Iterable), \
+            u'Values must be None or iterable'
+        self.values = values
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return None
+        values = list(map(lambda x: x[0], self.values))
+        return values.index(value)
+
+    def process_result_value(self, value, dialect):
+        return EnumInt(value, self.values)
